a7c135fb8d38dfc5efda90f69a369c6512be9a36
David Johnson-Davies <david@technoblogy.com>
2021-03-19 14:56:39 +0000

    Version 3.5b - 19th March 2021
    Uses flash for save-image on AVR DA- and DB-series

--- ulisp.ino
+++ ulisp.ino
@@ -1,5 +1,5 @@
-/* uLisp AVR Version 3.5a - www.ulisp.com
-   David Johnson-Davies - www.technoblogy.com - 4th March 2021
+/* uLisp AVR Version 3.5b - www.ulisp.com
+   David Johnson-Davies - www.technoblogy.com - 19th March 2021
 
    Licensed under the MIT license: https://opensource.org/licenses/MIT
 */
@@ -83,17 +83,19 @@ const char LispLibrary[] PROGMEM = "";
   #define CPU_ATmega4809
 
 #elif defined(__AVR_AVR128DA48__)
+  #include <Flash.h>
   #define Serial Serial1
-  #define WORKSPACESIZE 2800-SDSIZE       /* Objects (4*bytes) */
-  #define EEPROMSIZE 256                  /* Bytes */
+  #define WORKSPACESIZE (2800-SDSIZE)     /* Objects (4*bytes) */
+  #define FLASHWRITESIZE 16384            /* Bytes */
   #define SYMBOLTABLESIZE 256             /* Bytes */
   #define STACKDIFF 320
   #define CPU_AVR128DX48
 
 #elif defined(__AVR_AVR128DB48__)
+  #include <Flash.h>
   #define Serial Serial3
-  #define WORKSPACESIZE 2800-SDSIZE       /* Objects (4*bytes) */
-  #define EEPROMSIZE 256                  /* Bytes */
+  #define WORKSPACESIZE (2800-SDSIZE)     /* Objects (4*bytes) */
+  #define FLASHWRITESIZE 16384            /* Bytes */
   #define SYMBOLTABLESIZE 256             /* Bytes */
   #define STACKDIFF 320
   #define CPU_AVR128DX48
@@ -496,6 +498,26 @@ char *MakeFilename (object *arg) {
 void SDWriteInt (File file, int data) {
   file.write(data & 0xFF); file.write(data>>8 & 0xFF);
 }
+#elif defined(FLASHWRITESIZE)
+#define BASE_ADDRESS 0x1C000
+
+boolean FlashSetup () {
+  return (Flash.checkWritable() == FLASHWRITE_OK);
+}
+
+boolean FlashBeginWrite (int pages) {
+  // Erase how many 512K pages we need; must be a power of 2 <= 32.
+  return Flash.erasePage(BASE_ADDRESS, pages) == 0;
+}
+
+void FlashWriteByte (uint32_t *addr, uint8_t data) {
+  Flash.writeByte((*addr)++, data);
+}
+
+void FlashWriteInt (uint32_t *addr, int data) {
+  Flash.writeWord(*addr, data);
+  (*addr)++; (*addr)++;
+}
 #else
 void EEPROMWriteInt (unsigned int *addr, int data) {
   EEPROM.write((*addr)++, data & 0xFF); EEPROM.write((*addr)++, data>>8 & 0xFF);
@@ -529,6 +551,29 @@ unsigned int saveimage (object *arg) {
   }
   file.close();
   return imagesize;
+#elif defined(FLASHWRITESIZE)
+  if (!(arg == NULL || listp(arg))) error(SAVEIMAGE, invalidarg, arg);
+  if (!FlashSetup()) error2(SAVEIMAGE, PSTR("no DataFlash found."));
+  // Save to Flash
+  int SymbolUsed = SymbolTop - SymbolTable;
+  int bytesneeded = imagesize*4 + SymbolUsed + 10;
+  if (bytesneeded > FLASHWRITESIZE) error(SAVEIMAGE, PSTR("image too large"), number(imagesize));
+  if (!FlashBeginWrite(32)) error2(SAVEIMAGE, PSTR("problem erasing flash"));
+  uint32_t addr = BASE_ADDRESS;
+  FlashWriteInt(&addr, (uintptr_t)arg);
+  FlashWriteInt(&addr, imagesize);
+  FlashWriteInt(&addr, (uintptr_t)GlobalEnv);
+  FlashWriteInt(&addr, (uintptr_t)GCStack);
+  #if SYMBOLTABLESIZE > BUFFERSIZE
+  FlashWriteInt(&addr, (uintptr_t)SymbolTop);
+  for (int i=0; i<SymbolUsed; i++) FlashWriteByte(&addr, SymbolTable[i]);
+  #endif
+  for (unsigned int i=0; i<imagesize; i++) {
+    object *obj = &Workspace[i];
+    FlashWriteInt(&addr, (uintptr_t)car(obj));
+    FlashWriteInt(&addr, (uintptr_t)cdr(obj));
+  }
+  return imagesize;
 #else
   if (!(arg == NULL || listp(arg))) error(SAVEIMAGE, invalidarg, arg);
   int SymbolUsed = SymbolTop - SymbolTable;
@@ -556,6 +601,16 @@ unsigned int saveimage (object *arg) {
 int SDReadInt (File file) {
   uint8_t b0 = file.read(); uint8_t b1 = file.read();
   return b0 | b1<<8;
+}
+#elif defined(FLASHWRITESIZE)
+uint8_t FlashReadByte (uint32_t *addr) {
+  return Flash.readByte((*addr)++);
+}
+
+int FlashReadInt (uint32_t *addr) {
+  int data = Flash.readWord(*addr);
+  (*addr)++; (*addr)++;
+  return data;
 }
 #else
 int EEPROMReadInt (unsigned int *addr) {
@@ -589,6 +644,26 @@ unsigned int loadimage (object *arg) {
   file.close();
   gc(NULL, NULL);
   return imagesize;
+#elif defined(FLASHWRITESIZE)
+  if (!FlashSetup()) error2(LOADIMAGE, PSTR("no Flash found."));
+  uint32_t addr = BASE_ADDRESS;
+  FlashReadInt(&addr); // Skip eval address
+  int imagesize = FlashReadInt(&addr);
+  if (imagesize == 0 || imagesize == 0xFFFFFFFF) error2(LOADIMAGE, PSTR("no saved image"));
+  GlobalEnv = (object *)FlashReadInt(&addr);
+  GCStack = (object *)FlashReadInt(&addr);
+  #if SYMBOLTABLESIZE > BUFFERSIZE
+  SymbolTop = (char *)FlashReadInt(&addr);
+  int SymbolUsed = SymbolTop - SymbolTable;
+  for (int i=0; i<SymbolUsed; i++) SymbolTable[i] = FlashReadByte(&addr);
+  #endif
+  for (int i=0; i<imagesize; i++) {
+    object *obj = &Workspace[i];
+    car(obj) = (object *)FlashReadInt(&addr);
+    cdr(obj) = (object *)FlashReadInt(&addr);
+  }
+  gc(NULL, NULL);
+  return imagesize;
 #else
   unsigned int addr = 2; // Skip eval address
   unsigned int imagesize = EEPROMReadInt(&addr);
@@ -619,6 +694,13 @@ void autorunimage () {
   file.close();
   if (autorun != NULL) {
     loadimage(NULL);
+    apply(0, autorun, NULL, NULL);
+  }
+#elif defined(FLASHWRITESIZE)
+  uint32_t addr = BASE_ADDRESS;
+  object *autorun = (object *)FlashReadInt(&addr);
+  if (autorun != NULL && (unsigned int)autorun != 0xFFFF) {
+    loadimage(nil);
     apply(0, autorun, NULL, NULL);
   }
 #else
